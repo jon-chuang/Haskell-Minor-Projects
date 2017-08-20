a74450f55565cd73b2ba4d073740d5cb5969d1c3
kontentlos <jonabirdd@gmail.com>
2017-08-21 00:28:45 +0800

    Python 2 to 3 regex - converts print to print()

---
+++ python2to3.hs
@@ -0,0 +1,27 @@
+{-
+This program adds brackets around the expression following\
+\ any occurance of the python keyword 'print ' that follows\
+\ a new line and whitespace.\nDefault output location is directory of this program."
+-}
+module Python2to3 where
+
+import Data.String
+import System.IO
+import Text.Regex.Posix
+import Data.List
+
+brack fileLoc = do
+  fileString <- readFile fileLoc
+  outputData.(intercalate "\n").(map processData).lines $ fileString
+--intercalate is intersperse for lists (Data.List)
+
+processData :: String -> String
+processData line = let printpart = (line =~ print' :: String) in
+                        if ((line =~ print' :: Bool) && not (line =~ print'' :: Bool) )
+                            then printpart ++ "(" ++ drop (length printpart) line ++ ")"
+                            else line
+outputData :: String -> IO ()
+outputData processed = writeFile "output.py" processed
+
+print' = "[\t ]*print "
+print'' = "[\t ]*print[\t ]*[(]"
